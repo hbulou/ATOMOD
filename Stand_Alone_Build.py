c6dcd3de1e5c62ca4dcebd90fda0402ec03cbabe
hbulou <herve.bulou@ipcms.unistra.fr>
2026-03-06 14:37:40 +0100

    Modification de Stand_Alone_Build.py pour inclure la méthode core-shell() de la classe Crystal dans HBPy.

--- Stand_Alone_Build.py
+++ Stand_Alone_Build.py
@@ -8,12 +8,10 @@ def main():
     radius_NP=8.0  # ang
     N=int(radius_box/3.92)  # 3.92 est le parametre de maille du Pt en angstreom
     Bulk=Crystal()
-    r1=5.7
-    r2=7.0
-    r3=8.0
-    radius_comp=[r1,r2,r3]
-    composition=['Ir','Ru','Ni']
-    Bulk.build(Nx=N,Ny=N,Nz=N,elt=composition[0])
+
+
+
+    Bulk.build(Nx=N,Ny=N,Nz=N,elt='Pt')
     # découpe de la nanoparticule
     Bulk.origin_at_mass_center()
     molecule=Bulk.transform(radius=radius_NP)
@@ -22,25 +20,21 @@ def main():
     molecule.MassCenter()
     molecule.origin_at_mass_center()
     molecule.get_structure()
-    for atm in molecule.atoms:
-        d=atm.distance_from_(molecule.MC)
-        if d<= radius_comp[0]:
-            atm.elt=composition[0]
-        elif d<= radius_comp[1]:
-            atm.elt=composition[1]
-        else:
-            atm.elt=composition[-1]
-        print(atm.elt,atm.q,d)
+    composition=[
+        (5.7,'Ir'),
+        (7.0,'Ru'),
+        (float('inf'),'Ni')
+    ]
+    molecule.core_shell(composition)
+
     print(f"natom={len(molecule.atoms)}")
     molecule.get_element_distribution()
     for elt in molecule.list_elt:
         print(elt,len(molecule.pos_elt[elt]),100.0*len(molecule.pos_elt[elt])/len(molecule.atoms))
-    #for elt in composition:
-    #    if elt not in molecule.pos_elt:
-    #        molecule.pos_elt[elt]=[]
-    #        #print(f"### {elt} {molecule.pos_elt[elt]} {len(molecule.pos_elt[elt])} {len(molecule.atoms)}")
-    #        print(f"### {elt} {molecule.pos_elt[elt]} -> stoechiometry {len(molecule.pos_elt[elt])/len(molecule.atoms)}")
+
     molecule.save(prefix="NP")
+    molecule.mixing(nexchange=10*len(molecule.atoms),seed=0)
+    molecule.save(prefix="NPmix")
 # ##########################################################################################
 # Point d’entrée du programme
 if __name__ == "__main__":
