2ec7e0437e0bf5912b4114eb453a767b476d082a
hbulou <herve.bulou@ipcms.unistra.fr>
2026-04-15 21:02:59 +0200

    Développement d'une nouvelle version de Stand_Alone_ATOMOD.py from scratch. L'idée est d'intégrer les spectres EXAFS dans l'instruction d'ATOMOD, en plus des images TEM. Dans la version actuelle  * on synthétise la NP in silico,  * on calcule l'image TEM  * on calcules les cartes de probabilité de présence atomque (pas tout à fait terminé)

--- Stand_Alone_ATOMOD.py
+++ Stand_Alone_ATOMOD.py
@@ -1,225 +1,70 @@
-import os
-#os.environ["CUDA_VISIBLE_DEVICES"] = ""
-#import torch
-import tensorflow as tf
-#import torch
-from tensorflow.keras.models import Model,load_model
-from ATOMOD.ATOMOD import CustomDataGenerator,UNet,ImageSamplingCallback
-class NN:
-    def __init__(self):
-        self.batch_size=64
-        self.epochs=200000
-        #self.H=256
-        #self.W=256
-        self.H=64
-        self.W=64
-        #self.composition=['Fe','Pt']
-        self.composition=['Rh','Ir']
-        self.nz=10
-        self.restart=True
-        self.ATOMOD_Training_starting_model="unet_atomod_trained_last.h5"
-        self.initial_epochs=1
-        self.device="cuda"
-    def ATOMOD_training(self):
-        print("ATOMOD_training")
-        # Liste les GPU reconnus par TensorFlow
-        gpus = tf.config.list_physical_devices('GPU')
-
-        if gpus:
-            print(f"{len(gpus)} GPU(s) détecté(s) :")
-            for gpu in gpus:
-                print(f" - {gpu}")
-                self.device="cuda"
-        else:
-            print("Aucun GPU détecté, utilisation du CPU.")
-            self.device="cpu"
+import sys
+#import os
+import random
 
 
 
+import HBPy
+from HBPy.Molecule.Crystal import Crystal,Atom
 
+import matplotlib.pyplot as plt
+sys.path.append('./lib/')
+import abtem
 
-        #self.image_paths = sorted(glob(os.path.join(image_dir, "*")))
-        #if len(self.image_paths) == 0:
-        #    raise ValueError(f"Aucune image trouvée dans {image_dir}")
-        device=self.device
-        #if device is None:
-        #    device = "cuda" if torch.cuda.is_available() else "cpu"
-        print("Utilisation de l'appareil :", device)
-        
-                    
+import logging
+# Configuration du logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
-        # # Liste fictive des IDs d'images
-        # # En réalité, ceci serait généré par os.listdir(os.path.join(DATA_ROOT, 'images'))
+def main():
+    # _______________________________________
+    #
+    # Etape 1 : construire la nanoparticules
+    # _______________________________________
+    #   Etape 1.1 : la structure
+    NP=Crystal()
+    NP.build(radius=6.0,materials='NP')
+    NP.origin_at_mass_center()
+    logger.info(f"min={NP.qmin} max={NP.qmax}")
+    logger.info(f"Mass center={NP.MC}")
+    logger.info(f"Number of atoms={len(NP.atoms)}")
+    #   Etape 1.2 : la distribution chimique
+    composition=['Pt','Ni','Ir']
+    NP.set_composition(composition)
+    NP.save(prefix="NP",fmt='xyz')
+    #   Etape 1.3 : (optionnelle) l'optimisation structurale et/ou chimique
 
-        BATCH_SIZE=self.batch_size
-        EPOCHS=self.epochs
-        
+    # _______________________________________
+    #
+    # Etape 2 : construire l'image TEM
+    #   abTEM : https://github.com/abTEM/abTEM
+    #   https://abtem.readthedocs.io/en/latest/intro.html#
+    # _______________________________________
+    config={
+        'train':{
+            'TEM_img_dir':"data/train/images"  # répertoire de stockage des images TEM
+        },
+        'abtem':{
+            'dx':0.04,
+            'dy':0.04,
+            'dz':4.08/2,
+            'energy':300e3,
+            'focal spread':40,
+            'semiangle cutoff':20,
+            'defocus':200,
+            'cell scale':1.1
+            }
+    }
+    NP.abTEM(config)
+    # _______________________________________
+    #
+    # Etape 3 : construire les cartes de probabilité de présence atomique
+    # _______________________________________
 
-        # sélection des images/masks pour l'entrainement
-        train_IDs=[]
-        for i in range(BATCH_SIZE):
-            train_IDs.append(f'img_{i+1:04d}')
-
-        # sélection des images/masks pours la validation
-        val_IDs=[]
-        for i in range(BATCH_SIZE,2*BATCH_SIZE):
-            val_IDs.append(f'img_{i+1:04d}')
-        print(train_IDs)
-        print(val_IDs)
-        #for sp in self.composition:
-        #    f"{sp}_{i:04d}_{k:04d}.png"
-
-        DATA_ROOT="data/train"
-
-        # # --- 1. Initialisation des Générateurs ---
-        train_generator = CustomDataGenerator(
-             train_IDs, 
-             DATA_ROOT, 
-             (self.H, self.W), 
-             BATCH_SIZE, 
-             shuffle=True,
-            composition=self.composition,
-            nz=self.nz
-         )
-        
-        validation_generator = CustomDataGenerator(
-            val_IDs, 
-            DATA_ROOT, 
-            (self.H, self.W), 
-            BATCH_SIZE, 
-            shuffle=False,
-            composition=self.composition,
-            nz=self.nz
-        )
-
-        print("🆕 Création d'un nouveau modèle")
-        strategy = tf.distribute.MirroredStrategy()
-        print(f"Nombre de GPU utilisés : {strategy.num_replicas_in_sync}")
-        with strategy.scope():
-
-            self.model=UNet(self.H,self.W,len(self.composition)*self.nz)
-                # --- 3. Compilation du Modèle ---
-            if self.restart:
-                model_path = self.ATOMOD_Training_starting_model
-                if os.path.exists(model_path):
-                    print("🔄 Reprise de l'entraînement depuis", model_path)
-                    self.model = load_model(model_path, compile=False)
-
-                
-            self.model.compile(
-                optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4, clipnorm=1.0),
-                # Perte pour N classifications binaires indépendantes (H, W, N)
-                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5)],
-                run_eagerly=True
-            )
-        initial_epochs=self.initial_epochs
-        self.model.summary() # Décommenter pour voir l'architecture et la forme de sortie (None, H, W, 10)
-
-        # --- 4. Entraînement ---
-        tf.debugging.set_log_device_placement(True)
-        print("=== SÉCURITÉ GPU ===")
-        print("TF détecte :", tf.config.list_physical_devices('GPU'))
-        #print("PyTorch détecte :", torch.cuda.is_available())
-        #if torch.cuda.is_available():
-        #    print("Nom du GPU :", torch.cuda.get_device_name(0))
-        print("====================")
-        print("Démarrage de l'entraînement...")
-        # --- Préparation du Callback pour la Visualisation ---
-        # 1. Extraire un échantillon du générateur de validation
-        # On récupère le premier lot (batch) du générateur de validation
-        # Assurez-vous que votre CustomDataGenerator supporte l'indexation ([0]) ou utilisez next(iter(validation_generator))
-        try:
-            sample_batch_input, _ = validation_generator[0] 
-        except Exception:
-            # Si l'indexation n'est pas supportée, utilisez un itérateur :
-            sample_batch_input, _ = next(iter(validation_generator))
-
-        # 2. Sélectionner la première image du lot (Batch=1 pour la prédiction)
-        # La forme doit être (1, H, W, C) pour la prédiction UNet
-
-        intermediate_dir="data/train/intermediate"
-
-        
-        sample_input_for_callback = sample_batch_input[0:1]
-        callbacks_list=[]
-        callbacks_list.append(
-            ImageSamplingCallback(
-                sample_input_image=sample_input_for_callback, 
-                class_channel_index=0,
-                val_IDs=val_IDs,
-                nz=self.nz,
-                composition=self.composition,
-                output_dir=intermediate_dir,
-                H=self.H, 
-                W=self.W)
-        )
-            
-        #        Ce qui se passe à l’intérieur de fit#
-        #
-        #Pour chaque époque :
-        #
-        #for epoch in epochs:
-        #    for batch in data:
-        #        y_pred = model(x_batch, training=True)
-        #        loss = loss_fn(y_batch, y_pred)
-        #        gradients = tape.gradient(loss, weights)
-        #        optimizer.apply_gradients(...)
-        #        update_metrics()
-        #    run_validation()
-        #    callbacks.on_epoch_end()
-        #
-        # fit() encapsule tout cela automatiquement.
-
-        # --- VÉRIFICATION DU CHARGEMENT ---
-        print("\n--- TEST PRÉ-ENTRAÎNEMENT ---")
-        print("Vérification des performances actuelles du modèle...")
-
-        # On fait une évaluation sur un petit lot de validation AVANT d'entraîner
-        # Cela doit vous donner une loss proche de celle de votre dernier run
-        loss_initiale = self.model.evaluate(x_val, y_val, batch_size=GLOBAL_BATCH_SIZE, steps=5, verbose=1)
-
-        print(f"Loss au démarrage : {loss_initiale}")
-        print("-----------------------------\n")
-        
-        history = self.model.fit(
-            train_generator,
-            steps_per_epoch=len(train_generator),
-            epochs=EPOCHS,
-            initial_epoch=initial_epochs,
-            validation_data=validation_generator,
-            validation_steps=len(validation_generator),
-            callbacks=callbacks_list
-        )
-
-        print("Entraînement terminé.")
-        self.model.save('unet_atomod_trained.h5')
-        print("Modèle sauvegardé sous 'unet_atomod_trained.h5'")
-        
-        # Ce fichier contient :
-        #    l’architecture
-        #    les poids
-        #    l’optimizer avec son état interne (Adam, etc.)
-        
-        print("DONE!")
-
-# ##########################################################################################
-# Point d’entrée du programme
-if __name__ == "__main__":
-
-    # 1. Vérification simple (booléen)
-    #print(f"CUDA disponible : {torch.cuda.is_available()}")
-
-    # 2. Configuration du device dynamique
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #print(f"L'entraînement se fera sur : {device}")
-
-    # 3. Informations sur la carte (optionnel)
-    #if torch.cuda.is_available():
-    #    print(f"Nom du GPU : {torch.cuda.get_device_name(0)}")
-
-
-
+    NP.xyz2slice()
     
-    FePt=NN()
-    FePt.ATOMOD_training()
+if __name__ == "__main__":
+    main()
